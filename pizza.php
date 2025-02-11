c406c61159be577504a2da8a7bd6767ce260beda
iago-05 <15586823.clot@fje.edu>
2025-02-11 17:28:13 +0000

    Commit 5 de l'aplicatiu pizzes

--- pizza.php
+++ pizza.php
@@ -10,7 +10,7 @@
 			if (empty($_GET['ingr'])) {
 				$pvp = ((100 + $IVA) / 100) * $preu;
 				echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
-				exit(0);
+				#exit(0);
 			} else {
 				$IVA = 21;
 				$pvp = ((100 + $IVA) / 100) * ($preu + count($_GET['ingr']));
@@ -21,10 +21,11 @@
 					}
 				}
 				echo "El preu és $pvp € (IVA inclòs)<br>";
-				exit(0);
+				#exit(0);
 			}
 			echo "<br>";
 			echo "Autor: dacomo"."<br>";
 		?>
+		<a href="pizza.html">Retorna al formulari de selecció d’ingredients</a>
 	</body>
 </html>

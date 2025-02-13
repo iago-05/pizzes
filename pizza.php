cb230c3382008a9c4acb9302e2f2973f072a2200
iago-05 <15586823.clot@fje.edu>
2025-02-13 16:24:54 +0000

    Commit 7 de l’aplicatiu pizzes

--- pizza.php
+++ pizza.php
@@ -24,7 +24,8 @@
 				#exit(0);
 			}
 			echo "<br>";
-			echo "Autor: dacomo"."<br>";
+			echo "Autor: iavame"."<br>";
+			echo date("l jS \of F Y h:i:s A") . "<br>";
 		?>
 		<a href="pizza.html">Retorna al formulari de selecció d’ingredients</a>
 	</body>
